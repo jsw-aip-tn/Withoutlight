dc993e9e9d90a0b838416f160c0289978fc454f8
MichaelPovoa <134786590+MichaelPovoa@users.noreply.github.com>
2025-02-26 12:02:15 +0100

    little Change on the Start-Menu

--- Scripts/ScreensMenuScripts/winning_screen.gd
+++ Scripts/ScreensMenuScripts/winning_screen.gd
@@ -1,15 +1,5 @@
 extends CanvasLayer
 
-
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
 func _on_again_pressed() -> void:
 	Global.current_wave = 0
 	get_tree().change_scene_to_file("res://Scenes/main.tscn")
