816e18b43214f1cfc72b8bcb64a86b474f5d8fe3
justadudelsack <asheydrich@gmail.com>
2025-03-11 16:40:19 +0100

    adjusting script (wolf actually moves now :D)

--- Scripts/wolf.gd
+++ Scripts/wolf.gd
@@ -1,25 +1,31 @@
 extends CharacterBody2D
 
+var maxhp = 50
 var hp = 50
 var atk = 4
-const SPEED = 80.0
+@export var SPEED : float = 30
+var pushback_strength  = 0.5
 var player_in_range = false
 var direction = Vector2()
 var time_since_last_change = 0
-var change_interval = 1.0  # Alle ... Sekunden die Richtung ändern
+var change_interval = 2  # Alle 2 Sekunden die Richtung ändern
 var target
+var target_direction: Vector2 = Vector2.ZERO
+var dash_speed = 5
+var is_dashing = false
 @onready var progress_bar: ProgressBar = $ProgressBar2
-
 
 func _ready() -> void:
 	update_hpBar()
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	movement(delta)	
-
+	movement(delta)
+	
 func enemy():
 	pass
 	
+func update_hpBar():
+	progress_bar.update_health(hp, maxhp)
 	
 func movement(delta: float):
 	time_since_last_change += delta
@@ -52,4 +58,24 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 		print(hp)
 
 func _on_enemie_hitbox_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
+
+
+func _on_enemy_hitbox_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
+
+
+func _on_enemy_hitbox_body_exited(body: Node2D) -> void:
+	pass # Replace with function body.
+
+
+func _on_timer_timeout() -> void:
+	pass # Replace with function body.
+
+
+func _on_search_area_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
+
+
+func _on_search_area_body_exited(body: Node2D) -> void:
 	pass # Replace with function body.
