3fc31ecbb4fb27702a1dd11e5a07f973af55bc4a
justadudelsack <asheydrich@gmail.com>
2025-03-25 13:15:37 +0100

    Add movement wolf (#5)
    * visible HPbar added
    
    * adjusting script (wolf actually moves now :D)
    
    * further script update
    
    * wolf can take damage now :)
    
    * small changes
    
    * interrupted changes
    
    ---------
    
    Co-authored-by: MichaelPovoa <134786590+MichaelPovoa@users.noreply.github.com>

--- Scripts/wolf.gd
+++ Scripts/wolf.gd
@@ -1,55 +1,84 @@
 extends CharacterBody2D
 
+var maxhp = 50
 var hp = 50
 var atk = 4
-const SPEED = 80.0
+@export var SPEED : float = 30
+var pushback_strength  = 0.5
 var player_in_range = false
 var direction = Vector2()
 var time_since_last_change = 0
-var change_interval = 1.0  # Alle ... Sekunden die Richtung ändern
+var change_interval = 5  # Alle X Sekunden die Richtung ändern
 var target
-@onready var progress_bar: ProgressBar = $ProgressBar2
+var target_direction: Vector2 = Vector2.ZERO
+var dash_speed = 4
+var is_dashing = false
 
+@onready var progress_bar: ProgressBar = $ProgressBar2
+@onready var up: RayCast2D = $"Ray Casts/Up"
+@onready var down: RayCast2D = $"Ray Casts/Down"
+@onready var left: RayCast2D = $"Ray Casts/Left"
+@onready var right: RayCast2D = $"Ray Casts/Right"
 
 func _ready() -> void:
 	update_hpBar()
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	movement(delta)	
-
+	movement(delta)
+	
 func enemy():
 	pass
 	
+func update_hpBar():
+	progress_bar.update_health(hp, maxhp)
 	
 func movement(delta: float):
 	time_since_last_change += delta
-	if player_in_range:
-		var target_direction = target.position - position  # Vektor vom Gegner zum Spieler
-		direction = target_direction.normalized()
-	else:
+	if !player_in_range and !is_dashing:
 		if time_since_last_change >= change_interval:
 			var angle = randf_range(0, 2 * PI)  # Zufälligen Winkel generieren
-			direction = Vector2(cos(angle), sin(angle))  # Berechne Richtung
-			time_since_last_change = 0		
-	velocity = direction * SPEED 
-	move_and_slide()
+			direction = Vector2(cos(angle), sin(angle)) # Berechne Richtung
+			time_since_last_change = 0
+			velocity = direction.normalized() * SPEED	
+		move_and_slide()
+	if  player_in_range and is_dashing:
+		is_dashing = true
+		target_direction = (target.global_position - global_position).normalized()
+		velocity = target_direction * SPEED * dash_speed 
+		move_and_slide()
 	
 func received_damaged(atk):
 	hp = hp -  atk
+	update_hpBar()
 	if hp <= 0:
-		print("Dead")
 		queue_free()
 
-func _on_enemie_hitbox_body_exited(body: Node2D) -> void:
+func _on_enemie_hitbox_body_entered(body: Node2D) -> void:
 	if body.has_method("player"):
 		player_in_range = false
-
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body.has_method("player"):
-		player_in_range = true
+		is_dashing = false
 		target = body
 		body.received_damaged(atk)
-		print(hp)
+		body.pushback(position, pushback_strength)
 
-func _on_enemie_hitbox_body_entered(body: Node2D) -> void:
-	pass # Replace with function body.
+func _on_search_area_body_entered(body: Node2D) -> void:
+	if body.has_method("player"):
+		player_in_range = true
+		is_dashing = true
+		target = body
+		#attck_player(body)
+		#target_direction = target.position - position
+
+func _on_search_area_body_exited(body: Node2D) -> void:
+	if body.has_method("player"):
+		player_in_range = false
+		is_dashing = false
+		
+#func attck_player(body):
+#
+	#if player_in_range and !is_dashing:
+		#target_direction = (target.position - position).normalized()
+		#velocity = target_direction * SPEED * dash_speed 
+	#move_and_slide()
+	#print("Attack Moving")
+	
